56f869ae428279cab9fe975bc873dc90e3acfeca
Twoje Imię Nazwisko <twoj@email.com>
2024-07-17 23:34:50 +0200

    ArrayLinkedList modified

--- ArrayLinkedList.cpp
+++ ArrayLinkedList.cpp
@@ -1,91 +1,28 @@
 #include "ArrayLinkedList.h"
+#include <stdexcept>
 
-void ArrayLinkedList::resize(int new_capacity) {
-    Circle* new_data = new Circle[new_capacity];
-    int* new_next = new int[new_capacity];
-    for (int i = 0; i < capacity; ++i) {
-        new_data[i] = data[i];
-        new_next[i] = next[i];
-    }
-    delete[] data;
-    delete[] next;
-    data = new_data;
-    next = new_next;
-    for (int i = capacity; i < new_capacity; ++i) {
-        next[i] = i + 1;
-    }
-    next[new_capacity - 1] = -1;
-    freeIndex = capacity;
-    capacity = new_capacity;
-}
+ArrayLinkedList::ArrayLinkedList() {}
 
-ArrayLinkedList::ArrayLinkedList(int initial_capacity) {
-    size = 0;
-    capacity = initial_capacity;
-    data = new Circle[capacity];
-    next = new int[capacity];
-    for (int i = 0; i < capacity - 1; ++i) {
-        next[i] = i + 1;
-    }
-    next[capacity - 1] = -1;
-    head = -1;
-    freeIndex = 0;
-}
-
-ArrayLinkedList::~ArrayLinkedList() {
-    delete[] data;
-    delete[] next;
-}
+ArrayLinkedList::~ArrayLinkedList() {}
 
 void ArrayLinkedList::insert(const Circle& value) {
-    if (freeIndex == -1) {
-        resize(capacity * 2);
-    }
-    int newNode = freeIndex;
-    freeIndex = next[freeIndex];
-    data[newNode] = value;
-    next[newNode] = head;
-    head = newNode;
-    ++size;
+    data.push_back(value);
 }
 
 void ArrayLinkedList::remove(int index) {
-    if (index < 0 || index >= capacity) {
+    if (index < 0 || index >= data.size()) {
         throw std::out_of_range("Index out of range");
     }
-    if (head == -1) return;
-
-    if (head == index) {
-        int temp = head;
-        head = next[head];
-        next[temp] = freeIndex;
-        freeIndex = temp;
-    } else {
-        int current = head;
-        while (next[current] != -1 && next[current] != index) {
-            current = next[current];
-        }
-        if (next[current] == index) {
-            int temp = next[current];
-            next[current] = next[next[current]];
-            next[temp] = freeIndex;
-            freeIndex = temp;
-        }
-    }
-    --size;
+    data.erase(data.begin() + index);
 }
 
-Circle& ArrayLinkedList::get(int index) const {
-    if (index < 0 || index >= capacity) {
+Circle& ArrayLinkedList::get(int index) {
+    if (index < 0 || index >= data.size()) {
         throw std::out_of_range("Index out of range");
     }
     return data[index];
 }
 
 int ArrayLinkedList::getSize() const {
-    return size;
-}
-
-int ArrayLinkedList::getHead() const {
-    return head;
+    return data.size();
 }
--- ArrayLinkedList.h
+++ ArrayLinkedList.h
@@ -1,31 +1,21 @@
-#ifndef ARRAYBASEDLINKEDLIST_H
-#define ARRAYBASEDLINKEDLIST_H
+#ifndef ARRAYLINKEDLIST_H
+#define ARRAYLINKEDLIST_H
 
 #include "Circle.h"
-#include <stdexcept>
+#include <vector>
 
 class ArrayLinkedList {
 private:
     std::vector<Circle> data;
 
-    //Circle* data;
-    int* next;
-    int head;
-    int freeIndex;
-    int size;
-    int capacity;
-
-    void resize(int new_capacity);
-
 public:
-    ArrayLinkedList(int initial_capacity = 10);
+    ArrayLinkedList();
     ~ArrayLinkedList();
 
     void insert(const Circle& value);
     void remove(int index);
-    Circle& get(int index) const;
+    Circle& get(int index);
     int getSize() const;
-    int getHead() const;
 };
 
-#endif // ARRAYBASEDLINKEDLIST_H
+#endif // ARRAYLINKEDLIST_H
